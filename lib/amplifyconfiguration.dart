9a8971eee6f09dfa401d1e44e8176d081baaccbe
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-04-03 13:45:24 +0300

    ➕ Add Amplify and Amplify Auth

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,35 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-2:6788446c-4040-4743-a6c9-e5f814d3bee0",
+                            "Region": "us-east-2"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-2_5aFCdF4kb",
+                        "AppClientId": "6hdqs26d95j2filt9vp0enbbkv",
+                        "Region": "us-east-2"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_amplify/amplifyconfiguration.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,8 +22,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _amplifyConfigured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+
+    /// Add PinPoint and Cognito Plugins.
+    await Amplify.addPlugin(AmplifyAuthCognito());
+
+    /// Once Plugins are added, configure Amplify.
+    /// Note: Amplify can only be configured once.
+    try {
+      await Amplify.configure(amplifyconfig);
+
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } on AmplifyAlreadyConfiguredException catch (exception) {
+      print("Amplify was already configured. Was the app restarted?");
+      print("Amplify Exception: $exception");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text(
+            _amplifyConfigured ? "configured" : "not configured",
+          ),
+        ],
+      ),
+    );
   }
 }
